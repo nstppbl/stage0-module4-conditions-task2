b98812b92a23257aa81c35f735f9b888c16c9b60
nstppbl <1nstppbl@gmail.com>
2022-09-02 15:10:02 +0500

    initial commit

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if (first > second) {
+            System.out.print(first);
+        } else {
+            System.out.print(second);
+        }
     }
 }
